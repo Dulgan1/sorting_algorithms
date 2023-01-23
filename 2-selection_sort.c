aad89a2f27980046af36f2e58a7f8b1bfe0500b2
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-01-23 11:39:03 -0800

    Corrected mkstale

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,7 +20,7 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[min] > array[j])
-				min = j
+				min = j;
 		}
 		temp = array[i];
 		array[i] = array[min];
